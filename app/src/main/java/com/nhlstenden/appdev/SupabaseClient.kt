bad340ceb923bcd887c6c97dbd59fab60482d4e5
Mart Velema <mart.velema@student.nhlstenden.com>
2025-05-17 00:45:06 +0200

    Added User object, added fetching of User, simplified user creation function

--- app/src/main/java/com/nhlstenden/appdev/SupabaseClient.kt
+++ app/src/main/java/com/nhlstenden/appdev/SupabaseClient.kt
@@ -1,11 +1,15 @@
 package com.nhlstenden.appdev
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.RequestBody.Companion.toRequestBody
 import okhttp3.Response
+import org.json.JSONArray
 import org.json.JSONObject
+import java.util.UUID
 
 class SupabaseClient() {
     private val client = OkHttpClient()
@@ -13,21 +17,56 @@ class SupabaseClient() {
     private val supabaseUrl = "https://ggpdstbylyiwkfcucoxd.supabase.co"
     private val supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdncGRzdGJ5bHlpd2tmY3Vjb3hkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDczMDg4MzYsImV4cCI6MjA2Mjg4NDgzNn0.2ZGOttYWxBJkNcPmAtJh6dzlm3G6vwpIonEtRvtNNa8"
 
-    fun createNewUser(email: String, password: String): Response {
-        val signupRequest = this.signup(email, password)
+    fun createNewUser(email: String, password: String, username: String) {
+        val signupRequest = this.signup(email, password, username)
         if (signupRequest.code != 200) {
-            throw IllegalArgumentException("Unable to create a new user: " + signupRequest.body?.string() + "\nStatus code: " + signupRequest.code)
+            throw RuntimeException("Unable to create a new user: " + signupRequest.body?.string() + "\nStatus code: " + signupRequest.code)
         }
-        val loginRequest = login (email, password)
-        if (loginRequest.code != 200) {
-            throw IllegalArgumentException("Unable to create a new user: " + signupRequest.body?.string() + "\nStatus code: " + signupRequest.code)
+        val authToken = JSONObject(signupRequest.body?.string()).getString("access_token")
+        val createUserRequest = this.createUserAttributes(authToken)
+        if (createUserRequest.code != 201) {
+            throw RuntimeException("Unable to create new user properties: " + createUserRequest.body?.string() + "\nStatus code: " + createUserRequest.code)
         }
-        val authToken = JSONObject(loginRequest.body?.string()).getString("access_token")
-        return this.createUserAttributes(authToken)
     }
 
-    fun signup(email: String, password: String): Response {
-        val json = """{"email": "$email", "password": "$password"}"""
+    fun getUser(email: String, password: String): User {
+        val loginRequest = login(email, password)
+        if (loginRequest.code != 200) {
+            throw RuntimeException(loginRequest.body?.string())
+        }
+        val authResponse = JSONObject(loginRequest.body?.string())
+        val userRequest = this.getUserAttributes(authResponse.getString("access_token"))
+        if (userRequest.code != 200) {
+            throw RuntimeException(userRequest.body?.string())
+        }
+
+        val userResponse = JSONArray(userRequest.body?.string())
+        val friends = ArrayList<UUID>()
+        val jsonFriends = userResponse.getJSONObject(0).getJSONArray("friends")
+        for (i in 0 until jsonFriends.length()) {
+            friends.add(UUID.fromString(jsonFriends.getString(i)))
+        }
+
+        val achievements = ArrayList<String>()
+        val jsonAchievements = userResponse.getJSONObject(0).getJSONArray("achievements")
+        for (i in 0 until jsonAchievements.length()) {
+            achievements.add(jsonAchievements.getString(i))
+        }
+
+        return User(
+            authResponse.getString("access_token"),
+            UUID.fromString(authResponse.getJSONObject("user").getString("id")),
+            authResponse.getJSONObject("user").getJSONObject("user_metadata").getString("display_name"),
+            authResponse.getJSONObject("user").getString("email"),
+            userResponse.getJSONObject(0).getInt("points"),
+            friends,
+            achievements,
+            userResponse.getJSONObject(0).getString("profile_picture").toByteArray(),
+        )
+    }
+
+    fun signup(email: String, password: String, username: String): Response {
+        val json = """{"email": "$email", "password": "$password", "data": { "display_name": "$username" } }"""
         val requestBody = json.toRequestBody("application/json".toMediaType())
 
         val request = Request.Builder()
@@ -81,3 +120,15 @@ class SupabaseClient() {
         return client.newCall(request).execute()
     }
 }
+
+@Parcelize
+data class User(
+    val authToken: String,
+    val id: UUID,
+    val username: String,
+    val email: String,
+    val points: Int,
+    val friends: ArrayList<UUID>,
+    val achievements: ArrayList<String>,
+    val profilePicture: ByteArray
+) : Parcelable
