9446761463a60a2fe991e5f00f44ed63917da9ca
Dave <dave.van.den.berg@student.nhlstenden.com>
2025-05-20 20:42:15 +0200

    Made profile fragment show correct data again

--- app/src/main/java/com/nhlstenden/appdev/ProfileFragment.kt
+++ app/src/main/java/com/nhlstenden/appdev/ProfileFragment.kt
@@ -7,20 +7,7 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 
-/**
- * A simple [androidx.fragment.app.Fragment] subclass.
- * Use the [ProgressFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
 class ProfileFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -30,38 +17,20 @@ class ProfileFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val user = arguments?.getParcelable<UserResponse>("user")
 
         val usernameTextView = view.findViewById<TextView>(R.id.usernameTextView)
         val emailTextView = view.findViewById<TextView>(R.id.emailTextView)
-        usernameTextView.text = user?.username
-        emailTextView.text = "Email: ${user?.email}"
-    }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ProgressFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ProfileFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        val userData = arguments?.getParcelable<User>("USER_DATA")
+        userData?.let { user ->
+            usernameTextView.text = user.username
+            emailTextView.text = "Email: ${user.email}"
+        }
     }
 }
