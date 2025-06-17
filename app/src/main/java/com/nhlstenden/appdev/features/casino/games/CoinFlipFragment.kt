1e6a49612ee34db1f72199a4738264feb8dcb6a4
Mart Velema <mart.velema@student.nhlstenden.com>
2025-06-17 10:29:41 +0200

    Added casino pages

--- app/src/main/java/com/nhlstenden/appdev/features/casino/games/CoinFlipFragment.kt
+++ app/src/main/java/com/nhlstenden/appdev/features/casino/games/CoinFlipFragment.kt
@@ -1,7 +1,18 @@
 package com.nhlstenden.appdev.features.casino.games
 
+import com.nhlstenden.appdev.R
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
 class CoinFlipFragment : Fragment() {
-
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_casino_coin_flip, container, false)
+    }
 }
