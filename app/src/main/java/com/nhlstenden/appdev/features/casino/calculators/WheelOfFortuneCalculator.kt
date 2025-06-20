bd99b63ddf31f78f077ee3176c04ca2dda89aacc
Mart Velema <mart.velema@student.nhlstenden.com>
2025-06-20 21:38:55 +0200

    Fixed mixing of integers and doubles

--- app/src/main/java/com/nhlstenden/appdev/features/casino/calculators/WheelOfFortuneCalculator.kt
+++ app/src/main/java/com/nhlstenden/appdev/features/casino/calculators/WheelOfFortuneCalculator.kt
@@ -8,14 +8,14 @@ class WheelOfFortuneCalculator: ScoreCalculator {
             return score
         }
         return (score * when (additionalParams % 360) {
-            in 0.0..45.0 -> 5.0
-            in 45.1..90.0 -> 1.0
-            in 90.1..135.0 -> 2.0
-            in 135.1..180.0 -> 0.5
-            in 180.1..225.0 -> 0.0
-            in 225.1..270.0 -> 1.0
-            in 270.1..315.0 -> 2.0
-            in 315.1..360.0 -> 0.5
+            in 0..45 -> 5.0
+            in 45..90 -> 1.0
+            in 90..135 -> 2.0
+            in 135..180 -> 0.5
+            in 180..225 -> 0.0
+            in 225..270 -> 1.0
+            in 270..315 -> 2.0
+            in 315..360 -> 0.5
             else -> 1.0
         }).toInt()
     }
